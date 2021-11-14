11baf38bff2774ffff3ea78df5f22d601ae053fb
bob-lee <bob.bumsuk.lee@gmail.com>
2021-11-14 21:57:54 +1300

    watching array

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
+
+const INITIAL_NUMBERS = [0, 1, 2, 3, 4]
+const INITIAL_ITEMS = [
+  {
+    id: 0,
+    name: 'first item'
+  }
+]
 
 const { data } = await useAsyncData(
   'hello', 
@@ -10,18 +18,62 @@ const { data: images } = await useFetch('/api/images/portrait',
 )
 
 const hi = computed(() => data.value + '!')
-const count = computed(() => (images.value && Array.isArray(images.value)) ? images.value.length : 0)
+const countImages = computed(() => (images.value && Array.isArray(images.value)) ? images.value.length : 0)
 const imageToShow = ref('')
 const name = ref('Bob')
+const numbers = reactive(INITIAL_NUMBERS)
+const items = reactive(INITIAL_ITEMS)
+
+// watchEffect(() => console.table('watchEffect numbers', numbers))
+const countNumbers = ref(INITIAL_NUMBERS.length)
+const numbersString = ref(INITIAL_NUMBERS.toString())
+watch(
+  numbers, 
+  (numbers) => {
+    console.log('watch numbers', numbers)
+    countNumbers.value = numbers.length
+    numbersString.value = numbers.toString()
+  }
+)
+
+const countItems = ref(INITIAL_ITEMS.length)
+const firstName = ref(INITIAL_ITEMS[0].name)
+watch(
+  items, 
+  (items) => {
+    console.log('watch items', items)
+    countItems.value = items.length
+    firstName.value = items[0].name
+  }
+)
 
 console.log('hello:', hi.value)
-console.log('urls:', count.value)
+console.log('images:', countImages.value)
 
-watchEffect(() => console.log('watchEffect:', imageToShow.value))
-watch(imageToShow, (url, prevUrl) => console.log('watch', prevUrl || 'na', '->',  url || 'na'))
+watchEffect(() => console.log('imageToShow:', imageToShow.value))
+watch(
+  imageToShow, 
+  (url, prevUrl) => console.log('watch', prevUrl || 'na', '->',  url || 'na')
+)
 
 function urlClicked(url) {
   imageToShow.value = url
+}
+
+function addNumber() {
+  numbers.push(numbers.length)
+}
+
+function addItem() {
+  items.push({
+    id: items.length,
+    name: `I am item[${items.length}]`
+  })
+}
+
+function editItem() {
+  const item = items[0]
+  item.name += '!'
 }
 </script>
 
@@ -30,8 +82,15 @@ function urlClicked(url) {
     <h1>Home {{ data }}</h1>
 
     <p>hello api: {{ hi + ' ' + name }}</p>
+    <input type="text" v-model="name">
 
-    <pre>images api: {{ images[0] }}, {{ count }}</pre>
+    <pre>images api: {{ images[0] }}, {{ countImages }}, {{imageToShow || 'na' }}</pre>
+
+    <pre>watching numbers: {{countNumbers }}, [{{ numbersString }}]</pre>
+    <pre>watching items: {{countItems }}, [{{ firstName }}]</pre>
+    <button type="button" @click="addNumber">Add number</button>
+    <button type="button" @click="addItem">Add item</button>
+    <button type="button" @click="editItem">Edit item</button>
 
     <div v-for="image in images" :key="image.url">
       <div @click="urlClicked(image.url)"> {{ image.fileName }}</div>
@@ -39,7 +98,6 @@ function urlClicked(url) {
 
     <br>
 
-    <input type="text" v-model="name">
 
     <SingleImage :url="imageToShow" @close="imageToShow = ''" />
   </div>
